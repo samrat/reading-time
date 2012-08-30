8eadb71e0342079461878c9e934d0321bdc0a826
Samrat Man Singh <samratmansingh@gmail.com>
2012-08-30 08:04:57 +0545

    change project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject reading-time "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+            :description "Reading Time"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [clojurewerkz/crawlista "1.0.0-alpha17"]
                            [cheshire "4.0.2"]
