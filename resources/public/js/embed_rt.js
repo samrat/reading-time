3e92ad12e70f60fbe9743294bdb38d6725db7d71
Samrat Man Singh <samratmansingh@gmail.com>
2012-08-30 23:47:55 +0545

    Reading time from javascript

--- resources/public/js/embed_rt.js
+++ resources/public/js/embed_rt.js
@@ -1,5 +1,6 @@
 $.getJSON("http://www.reading-time.samrat.me/api?url=" + location.href + "&callback=?",
 	{},
 	function(data) {
+	  $("div.rt_readable").append("Reading Time:");
 	  $("div.rt_readable").append(data.readable);
 	});
